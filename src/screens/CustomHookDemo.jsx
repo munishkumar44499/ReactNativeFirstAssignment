7c253737b66167af0ee3e7c57021072f293766cc
munish <munishkumar44499@gmail.com>
2025-09-14 21:20:14 +0530

    Custom Hook Demo format

--- src/screens/CustomHookDemo.jsx
+++ src/screens/CustomHookDemo.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import { SafeAreaView } from "react-native";
 import UsersList from "./UsersList.js";
 
-const CustomHookDemo=() => {
-  return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <UsersList />
-    </SafeAreaView>
-  );
+const CustomHookDemo = () => {
+    return (
+        <SafeAreaView style={{ flex: 1 }}>
+            <UsersList />
+        </SafeAreaView>
+    );
 }
 
 export default CustomHookDemo
